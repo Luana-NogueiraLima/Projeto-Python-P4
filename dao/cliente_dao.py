8e79c0cc2a5b7ffaba087e6f9acf89b857b00339
Luana Nogueira de Lima <luana.nogueiralima99@gmail.com>
2025-11-06 09:48:16 -0300

    Create cliente_dao.py

---
+++ dao/cliente_dao.py
@@ -0,0 +1,15 @@
+class ClienteDAO:
+    def __init__(self, conexao):
+        self.conexao = conexao
+
+    def inserir(self, cliente):
+        pass
+
+    def listar(self):
+        pass
+
+    def atualizar(self, cliente):
+        pass
+
+    def excluir(self, id):
+        pass
