b52fdda91281d3565ab732dfa18e253a67a106c8
Luana Nogueira de Lima <luana.nogueiralima99@gmail.com>
2025-11-06 09:42:31 -0300

    Create cliente.py

---
+++ model/model/cliente.py
@@ -0,0 +1,6 @@
+class Cliente:
+    def __init__(self, id=None, nome=None, email=None, telefone=None):
+        self.id = id
+        self.nome = nome
+        self.email = email
+        self.telefone = telefone
