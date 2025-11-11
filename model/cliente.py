d453b0bc2d4d7c134bfbd728f714088438ea04a8
Luana Nogueira de Lima <luana.nogueiralima99@gmail.com>
2025-11-11 10:56:25 -0300

    Update cliente.py

--- model/cliente.py
+++ model/cliente.py
@@ -4,3 +4,31 @@ class Cliente:
         self.nome = nome
         self.email = email
         self.telefone = telefone
+
+
+    def get_id(self):
+        return self.__id
+
+    def set_id(self, id):
+        self.__id = id
+
+    def get_nome(self):
+        return self.__nome
+
+    def set_nome(self, nome):
+        self.__nome = nome
+
+    def get_email(self):
+        return self.__email
+
+    def set_email(self, email):
+        self.__email = email
+
+    def get_telefone(self):
+        return self.__telefone
+
+    def set_telefone(self, telefone):
+        self.__telefone = telefone
+
+    def __str__(self):
+        return f"{self.__id} - {self.__nome} | {self.__email} | {self.__telefone}"
