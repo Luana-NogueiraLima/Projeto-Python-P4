6dc6ebaf146bc32c82ca531900aef6a2234b28f1
Luana Nogueira de Lima <luana.nogueiralima99@gmail.com>
2025-11-11 11:08:18 -0300

    Update conexao.py

--- database/conexao.py
+++ database/conexao.py
@@ -3,3 +3,16 @@ import sqlite3
 def criar_conexao():
     conexao = sqlite3.connect('clientes.db')
     return conexao
+
+def criar_tabela(conexao):
+    cursor = conexao.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS cliente (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT NOT NULL,
+            email TEXT NOT NULL,
+            telefone TEXT NOT NULL
+        )
+    ''')
+    conexao.commit()
+    
