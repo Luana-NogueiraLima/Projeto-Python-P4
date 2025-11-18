8a46643e46fb9af730a76d8bf20aaae2ebb838ef
Luana Nogueira de Lima <luana.nogueiralima99@gmail.com>
2025-11-17 21:07:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
-from dao.cliente_dao import ClienteDAO
-from database.conexao import criar_conexao
 
-def menu():
-    pass
-
-def main():
-    conexao = criar_conexao()
-    cliente_dao = ClienteDAO(conexao)
-    menu()
-
-if __name__ == "__main__":
-    main()
 from dao.cliente_dao import ClienteDAO
 from model.cliente import Cliente
 from database.conexao import criar_conexao, criar_tabela
