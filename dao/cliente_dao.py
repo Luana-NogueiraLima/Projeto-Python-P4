ea663ccd889967a5e5792df414a32d532f92a852
Luana Nogueira de Lima <luana.nogueiralima99@gmail.com>
2025-11-17 21:06:34 -0300

    Update cliente_dao.py

--- dao/cliente_dao.py
+++ dao/cliente_dao.py
@@ -1,18 +1,3 @@
-class ClienteDAO:
-    def __init__(self, conexao):
-        self.conexao = conexao
-
-    def inserir(self, cliente):
-        pass
-
-    def listar(self):
-        pass
-
-    def atualizar(self, cliente):
-        pass
-
-    def excluir(self, id):
-        pass
 
 from model.cliente import Cliente
 
